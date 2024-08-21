2a7b150e41d8cb2580ffbcbdf120aa8984c8bb6e
Richelle helena <151651845+richelle-2@users.noreply.github.com>
2024-08-21 18:04:34 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+  
+    let numeroUm = 1
+    let stringUm = '1'
+    let numeroTrinta = 30
+    let stringTrinta = '30'
+    let numeroDez = 10
+    let stringDez = '10'
+
+
+    if(numeroUm == stringUm) {
+        console.log('As variaveis numeroUm e stringUm tem mesmo valor, mais tipos dierentes')
+    } else {
+        console.log('As variaveis numeroUm e stringUm nao tem o mesmo valor')
+    }
+     if(numeroTrinta === stringTrinta) {
+        console.log('As variaveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+
+    } else {
+        console.log('As variaveis numeroTrinta e stringTrinta nao tem o mesmo tipo')
+    } 
+    if(numeroDez == stringDez){
+        console.log('As variaveis numeroDez e stringDez tem o mesmo valor, mas tipos dierentes')
+
+    } else {
+        console.log('As variaveis numeroDez e strinDez nao tem o mesmo valor')
+    }
